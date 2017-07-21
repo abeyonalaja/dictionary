d99ed812b9fb8eaf79ffe34c751a76f6c26f2c90
Abideen Onalaja <abideenonalaja@Abideens-MacBook-Pro.local>
2017-07-21 11:48:34 +0300

    Clean up

--- lib/dictionary.ex
+++ lib/dictionary.ex
@@ -3,16 +3,15 @@ defmodule Dictionary do
   Documentation for Dictionary.
   """
 
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Dictionary.hello
-      :world
-
-  """
-  def hello do
-    :world
+  def word_list do
+    "assets/words.txt"
+    |> File.read!()
+    |> String.split(~r/\n/)
   end
+
+  def random_word do
+    word_list()
+    |> Enum.random()
+  end
+
 end
